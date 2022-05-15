97059299ce4c0d406bdc22e7d253a7e9f9744ecd
km7865 <rjsxo789@naver.com>
2022-05-15 20:36:30 +0900

    initial commit

---
+++ src/main/java/webserver/RequestHandler.java
@@ -0,0 +1,55 @@
+package webserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class RequestHandler {
+
+    private Socket connection;
+
+    public RequestHandler(Socket connection) {
+        this.connection = connection;
+    }
+
+    public void run() {
+        log.debug("New Client Connected! Connected IP: {}, Port: {}"
+                , connection.getInetAddress(), connection.getPort());
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // TODO
+
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/webserver/WebServer.java
@@ -0,0 +1,33 @@
+package webserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Slf4j
+public class WebServer {
+    private static final int DEFAULT_PORT = 8088;
+
+    public static void main(String[] args) throws Exception {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server started {} port.", port);
+
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.run();
+            }
+        }
+    }
+
+}
